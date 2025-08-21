0fdd610bf3f5a1461dc08c14b41a61a27e61060e
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-08-21 11:38:20 +0200

    chore: Bump to core 16.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["InfomaniakNotifications"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "15.0.0")),
+        .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "16.0.0")),
     ],
     targets: [
         .target(
